36a39102942daeb69997a0dcddbf3afc7156fe43
Bob Belderbos <bbelderbos@users.noreply.github.com>
2019-04-27 02:28:48 +0200

    Update driving.py

--- 101/driving.py
+++ 101/driving.py
@@ -1,4 +1,4 @@
 MIN_DRIVING_AGE = 18
 
 def allowed_driving(name, age):
-    print(f'{name} is{(" not","")[age >= MIN_DRIVING_AGE]} allowed to drive')
+    print(f'{name} is{(" not","")[age >= MIN_DRIVING_AGE]} allowed to drive!')
